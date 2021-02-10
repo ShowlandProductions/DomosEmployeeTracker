b5ef7217f99f992346f5207bc42c8c5b9dbdb504
Dominique Christopher <showlandproductions@gmail.com>
2021-02-09 17:24:09 -0800

    Add files via upload
    Added styling to app and Get functions for departments, roles, managers, & employees

--- server.js
+++ server.js
@@ -16,4 +16,74 @@ var connection = mysql.createConnection({
     password: "Padstowlane2!",
     database: "employees_db"
   });
-  
+  figlet('Showland Productions Employee Tracker', (err, result) => {
+    console.log(err || result);
+  });
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+    getDepartments();
+    getRoles();
+    getManagers();
+    getEmployees();
+  });
+  start = () => {
+    inquirer
+      .prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"]
+      })
+      .then(function(answer) {
+        if (answer.choices === "ADD") {
+          addSomething();
+        }
+        else if (answer.choices === "VIEW") {
+          viewSomething();
+        } 
+        else if (answer.choices === "UPDATE") {
+          updateSomething();
+        }
+        else if (answer.choices === "DELETE") {
+          deleteSomething();
+        }
+        else if (answer.choices === "EXIT") {
+          figlet('Thanks for using FSC Employee Tracker', (err, result) => {
+            console.log(err || result);
+          });
+          connection.end();
+        }
+        else{
+          connection.end();
+        }
+      });
+  }
+getRoles = () => {
+  connection.query("SELECT id, title FROM role", (err, res) => {
+    if (err) throw err;
+    roles = res;
+    // console.table(roles);
+  })
+};
+getDepartments = () => {
+  connection.query("SELECT id, name FROM department", (err, res) => {
+    if (err) throw err;
+    departments = res;
+    // console.log(departments);
+  })
+};
+getManagers = () => {
+  connection.query("SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee", (err, res) => {
+    if (err) throw err;
+    managers = res;
+    // console.table(managers);
+  })
+};
+getEmployees = () => {
+  connection.query("SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee", (err, res) => {
+    if (err) throw err;
+    employees = res;
+    // console.table(employees);
+  })
+};
